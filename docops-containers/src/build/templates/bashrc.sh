2029e21f62638e7f6241f26734bdfeceaa800450
Naomi Rose <23469+nomiro@users.noreply.github.com>
2022-03-05 12:37:07 +0000

    Configure git to pull using rebase

--- docops-containers/src/build/templates/bashrc.sh
+++ docops-containers/src/build/templates/bashrc.sh
@@ -7,6 +7,9 @@ eval "$(dircolors -p |
     sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' |
     dircolors /dev/stdin)" || true
 
+# Configure Git
+git config pull.rebase true
+
 # Source `.profile` (for `PATH` additions) without causing an infinite loop
 # https://github.com/koalaman/shellcheck/wiki/SC1091
 BASH_VERSION="" . "${HOME}/.profile"
